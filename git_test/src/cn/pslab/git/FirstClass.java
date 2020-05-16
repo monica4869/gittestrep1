3c65f34ffbc380e923c314adec1f83fc4d0167b7
monica <monica@163.com>
2020-05-16 17:06:51 +0800

    修改firstclass

--- git_test/src/cn/pslab/git/FirstClass.java
+++ git_test/src/cn/pslab/git/FirstClass.java
@@ -3,10 +3,12 @@ package cn.pslab.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello git2 !");
     }
 
     public void test(){
         int i = 66;
         System.out.println(i);
+        System.out.println("hello i");
     }
 }
