330617c5419f7c16566dd864519940b197b035eb
monica <monica@163.com>
2020-05-16 16:44:46 +0800

    把本地工程添加到git

--- git_test/src/cn/pslab/git/FirstClass.java
+++ git_test/src/cn/pslab/git/FirstClass.java
@@ -4,4 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git !");
     }
+
+    public void test(){
+        int i = 66;
+        System.out.println(i);
+    }
 }
